25e0b8a6b4ad52ee6344dd4aa9b180afa0bbeec1
MaxZay <maximzay2017@gmail.com>
2021-04-24 20:10:04 +0300

    feat: add change, mousemove and fullscreen events

---
+++ photo-filter/script.js
@@ -0,0 +1,38 @@
+let inputs = document.querySelectorAll('input')
+let results = document.querySelectorAll('output')
+let reset = document.querySelector('.btn-reset')
+let fullScreenButton = document.querySelector('.fullscreen')
+
+inputs.forEach(input => input.addEventListener('change', mouseEvent))
+inputs.forEach(input => input.addEventListener('mousemove', mouseEvent))
+
+reset.addEventListener('click', resetEvent)
+
+fullScreenButton.addEventListener('click', () => {
+    document.fullscreen == false ? document.documentElement.requestFullscreen() : document.exitFullscreen()
+})
+
+
+function mouseEvent() {
+    this.parentElement.children[1].value = this.value
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + this.dataset.sizing)
+}
+
+function resetEvent() {
+    document.documentElement.style.setProperty(`--blur`, 0)
+    document.documentElement.style.setProperty(`--invert`, 0)
+    document.documentElement.style.setProperty(`--sepia`, 0)
+    document.documentElement.style.setProperty(`--saturate`, 1)
+    document.documentElement.style.setProperty(`--hue`, 0)
+    inputs[0].value = 0
+    inputs[1].value = 0
+    inputs[2].value = 0
+    inputs[3].value = 100
+    inputs[4].value = 0
+    results[0].value = 0;
+    results[1].value = 0;
+    results[2].value = 0;
+    results[3].value = 100;
+    results[4].value = 0;
+
+}
