57de422eaf536f1674c5cd98625efb70e5582d44
MaxZay <maximzay2017@gmail.com>
2021-04-26 21:56:02 +0300

    refactor: change download button

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -8,7 +8,7 @@ let load = document.querySelector('#btnInput')
 let next = document.querySelector('.btn-next')
 let srcImage = './assets/img/img.jpg'
 console.log(canvasImg);
-
+const ctx = canvasImg.getContext('2d')
 const day = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/'
 const evening = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/'
 const morning = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/morning/'
@@ -16,7 +16,6 @@ const night = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-ta
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 
 let currentIndex = 0;
-
 onLoadImg(srcImage)
 
 inputs.forEach(input => input.addEventListener('change', mouseEvent))
@@ -29,16 +28,42 @@ fullScreenButton.addEventListener('click', () => {
 })
 
 download.addEventListener('click', function () {
+    // const image = new Image()
+    // image.setAttribute('crossOrigin', 'anonymous')
+    // image.src = srcImage
+    // const ctx = canvasImg.getContext('2d')
+    // ctx.filter = `blur(${inputs[0].value}px) invert(${inputs[1].value}%) sepia(${inputs[2].value}%) saturate(${inputs[3].value}%) hue-rotate(${inputs[4].value}deg)`
+    // ctx.drawImage(image, 0, 0)
+    // var link = document.createElement('a');
+    // link.download = 'download.png';
+    // link.href = canvasImg.toDataURL();
+    // link.click();
+    // document.documentElement.style.setProperty(`--blur`, inputs[0].value + 'px')
+    // document.documentElement.style.setProperty(`--invert`, inputs[1].value + '%')
+    // document.documentElement.style.setProperty(`--sepia`, inputs[2].value + '%')
+    // document.documentElement.style.setProperty(`--saturate`, inputs[3].value + '%')
+    // document.documentElement.style.setProperty(`--hue`, inputs[4].value + 'deg')
+    // download.value = ''
     const image = new Image()
-    image.src = srcImage
     image.setAttribute('crossOrigin', 'anonymous')
-    const ctx = canvasImg.getContext('2d')
-    ctx.filter = `blur(${inputs[0].value}px) invert(${inputs[1].value}%) sepia(${inputs[2].value}%) saturate(${inputs[3].value}%) hue-rotate(${inputs[4].value}deg)`
-    ctx.drawImage(image, 0, 0)
-    var link = document.createElement('a');
-    link.download = 'download.png';
-    link.href = canvasImg.toDataURL();
-    link.click();
+    image.src = srcImage
+    image.onload = function () {
+        const ctx = canvasImg.getContext('2d')
+        ctx.clearRect(0, 0, canvasImg.width, canvasImg.height)
+        ctx.filter = `blur(${inputs[0].value}px) invert(${inputs[1].value}%) sepia(${inputs[2].value}%) saturate(${inputs[3].value}%) hue-rotate(${inputs[4].value}deg)`
+        console.log(image);
+        ctx.drawImage(image, 0, 0)
+        console.log(ctx.filter);
+        // var link = document.createElement('a');
+        // link.download = 'download.png';
+        // link.href = canvasImg.toDataURL();
+        // link.click();
+        // document.documentElement.style.setProperty(`--blur`, `${inputs[0].value}px`)
+        // document.documentElement.style.setProperty(`--invert`, `${inputs[1].value}%`)
+        // document.documentElement.style.setProperty(`--sepia`, `${inputs[2].value}%`)
+        // document.documentElement.style.setProperty(`--saturate`, `${inputs[3].value}%`)
+        // document.documentElement.style.setProperty(`--hue`, `${inputs[4].value}deg`)
+    }
 })
 
 load.addEventListener('change', function (e) {
@@ -74,12 +99,12 @@ next.addEventListener('click', function () {
 function onLoadImg(src) {
     srcImage = src
     let img = new Image()
-    const ctx = canvasImg.getContext('2d')
     img.setAttribute('crossOrigin', 'anonymous')
     img.src = src
     img.onload = function () {
         canvasImg.width = img.width
         canvasImg.height = img.height
+        const ctx = canvasImg.getContext('2d')
         ctx.drawImage(img, 0, 0)
     }
 }
@@ -90,11 +115,11 @@ function mouseEvent() {
 }
 
 function resetEvent() {
-    document.documentElement.style.setProperty(`--blur`, 0)
-    document.documentElement.style.setProperty(`--invert`, 0)
-    document.documentElement.style.setProperty(`--sepia`, 0)
-    document.documentElement.style.setProperty(`--saturate`, 1)
-    document.documentElement.style.setProperty(`--hue`, 10)
+    document.documentElement.style.setProperty(`--blur`, 0 + 'px')
+    document.documentElement.style.setProperty(`--invert`, 0 + '%')
+    document.documentElement.style.setProperty(`--sepia`, 0 + '%')
+    document.documentElement.style.setProperty(`--saturate`, 100 + '%')
+    document.documentElement.style.setProperty(`--hue`, 0 + 'deg')
     inputs[0].value = 0
     inputs[1].value = 0
     inputs[2].value = 0
