a0233d8c4ded3bf8616dd0adc6a1badd752b1d17
MaxZay <maximzay2017@gmail.com>
2021-04-26 18:57:26 +0300

    feat: add download image with filters

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -6,17 +6,18 @@ let canvasImg = document.querySelector('canvas')
 let download = document.querySelector('.btn-save')
 let load = document.querySelector('#btnInput')
 let next = document.querySelector('.btn-next')
+let srcImage = './assets/img/img.jpg'
+console.log(canvasImg);
 
 const day = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/'
 const evening = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/'
 const morning = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/morning/'
 const night = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/'
-
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 
 let currentIndex = 0;
 
-onLoadImg('./assets/img/img.jpg')
+onLoadImg(srcImage)
 
 inputs.forEach(input => input.addEventListener('change', mouseEvent))
 inputs.forEach(input => input.addEventListener('mousemove', mouseEvent))
@@ -28,11 +29,16 @@ fullScreenButton.addEventListener('click', () => {
 })
 
 download.addEventListener('click', function () {
+    const image = new Image()
+    image.src = srcImage
+    image.setAttribute('crossOrigin', 'anonymous')
+    const ctx = canvasImg.getContext('2d')
+    ctx.filter = `blur(${inputs[0].value}px) invert(${inputs[1].value}%) sepia(${inputs[2].value}%) saturate(${inputs[3].value}%) hue-rotate(${inputs[4].value}deg)`
+    ctx.drawImage(image, 0, 0)
     var link = document.createElement('a');
     link.download = 'download.png';
     link.href = canvasImg.toDataURL();
     link.click();
-    link.delete;
 })
 
 load.addEventListener('change', function (e) {
@@ -60,10 +66,23 @@ next.addEventListener('click', function () {
         onLoadImg(night + images[currentIndex])
         currentIndex++;
     }
+    if (currentIndex == 20)
+        currentIndex = 0
 
 })
 
-
+function onLoadImg(src) {
+    srcImage = src
+    let img = new Image()
+    const ctx = canvasImg.getContext('2d')
+    img.setAttribute('crossOrigin', 'anonymous')
+    img.src = src
+    img.onload = function () {
+        canvasImg.width = img.width
+        canvasImg.height = img.height
+        ctx.drawImage(img, 0, 0)
+    }
+}
 
 function mouseEvent() {
     this.parentElement.children[1].value = this.value
@@ -87,16 +106,4 @@ function resetEvent() {
     results[3].value = 100;
     results[4].value = 0;
 
-}
-
-function onLoadImg(src) {
-    let img = new Image()
-    img.setAttribute('crossOrigin', 'anonymous')
-    img.src = src
-    img.onload = function () {
-        canvasImg.width = img.width
-        canvasImg.height = img.height
-        const ctx = canvasImg.getContext('2d')
-        ctx.drawImage(img, 0, 0)
-    }
 }
